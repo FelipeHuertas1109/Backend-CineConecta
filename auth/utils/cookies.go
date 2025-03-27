70f8b6f09131306ae19205d210e1e9d73c9f80b9
Sebastian Hogar <juanromero2719@gmail.com>
2025-03-27 01:45:29 -0500

    fix: cookie

--- auth/utils/cookies.go
+++ auth/utils/cookies.go
@@ -9,7 +9,7 @@ import (
 )
 
 func SetTokenCookie(c *gin.Context, token string) {
-	isProduction := os.Getenv("VERCEL") != ""
+	isProduction := os.Getenv("ENV") == "production"
 
 	cookie := &http.Cookie{
 		Name:     "cine_token",
@@ -19,11 +19,10 @@ func SetTokenCookie(c *gin.Context, token string) {
 		Secure:   true,
 		SameSite: http.SameSiteNoneMode,
 		MaxAge:   int((24 * time.Hour).Seconds()),
-		Domain:   "*",
 	}
 
 	if isProduction {
-		cookie.Domain = "*"
+		cookie.Domain = ""
 	}
 
 	http.SetCookie(c.Writer, cookie)
