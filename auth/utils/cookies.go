e7b4fe23243100fefb3fb9fa0c0c9be4d1c7d0af
Sebastian Hogar <juanromero2719@gmail.com>
2025-03-27 08:41:24 -0500

    fix: cookie

--- auth/utils/cookies.go
+++ auth/utils/cookies.go
@@ -8,14 +8,13 @@ import (
 )
 
 func SetTokenCookie(c *gin.Context, token string) {
-
 	cookie := &http.Cookie{
 		Name:     "cine_token",
 		Value:    token,
 		Path:     "/",
-		HttpOnly: false,
-		Secure:   true,
-		SameSite: http.SameSiteNoneMode,
+		HttpOnly: true,                  // Permitir acceso desde JavaScript
+		Secure:   true,                  // Siempre usar HTTPS
+		SameSite: http.SameSiteNoneMode, // Permitir cross-site
 		MaxAge:   int((24 * time.Hour).Seconds()),
 	}
 
