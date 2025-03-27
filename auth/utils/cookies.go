d9985632fbf452f0d6e25aed7b4d70325dc3b380
Sebastian Hogar <juanromero2719@gmail.com>
2025-03-27 01:50:17 -0500

    fix: cookie

--- auth/utils/cookies.go
+++ auth/utils/cookies.go
@@ -2,14 +2,12 @@ package utils
 
 import (
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetTokenCookie(c *gin.Context, token string) {
-	isProduction := os.Getenv("ENV") == "production"
 
 	cookie := &http.Cookie{
 		Name:     "cine_token",
@@ -19,10 +17,6 @@ func SetTokenCookie(c *gin.Context, token string) {
 		Secure:   true,
 		SameSite: http.SameSiteNoneMode,
 		MaxAge:   int((24 * time.Hour).Seconds()),
-	}
-
-	if isProduction {
-		cookie.Domain = ""
 	}
 
 	http.SetCookie(c.Writer, cookie)
